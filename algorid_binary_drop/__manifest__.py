7b2c1cf3a55ac665bad4227853b5097d023ca502
gmostafa1210 <gmostafa1210@gmail.com>
2026-09-22 15:21:00 +0600

    [ADD] algorid_binary_drop/

---
+++ algorid_binary_drop/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+#################################################################
+#                                                               #
+#       Developed by Algorid                                    #
+#       Copyright (C) 2026 - Today (https://www.algorid.com)    #
+#       Contact us: contact@algorid.com                         #
+#                                                               #
+#################################################################
+
+{
+    'name': 'Binary Drop Widget',
+    'author': 'Algorid Limited',
+    'website': 'https://www.algorid.com',
+    'company': 'Algorid Limited',
+    'maintainer': 'Algorid Limited',
+    'category': 'Tools',
+    'version': '18.0.1.0.0',
+    'summary': 'Extends the binary widget so a file can be dropped on the field',
+    'description': """
+        Provides a new field widget called 'binary_drop' that works exactly like
+        the standard binary widget but turns the field into a drop area: a file
+        dragged from the desktop and dropped on it is uploaded straight away.
+        The usual Upload / Download / Edit / Clear buttons are kept, and the
+        accepted_file_extensions option is honoured for dropped files too.
+    """,
+    'license': 'LGPL-3',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'algorid_binary_drop/static/src/scss/binary_drop_field.scss',
+            'algorid_binary_drop/static/src/xml/binary_drop_field.xml',
+            'algorid_binary_drop/static/src/js/binary_drop_field.js',
+        ],
+    },
+    'images': [
+        'static/description/banner.png'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
