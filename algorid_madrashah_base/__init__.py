d56453cc7e692ae00eeea25bd518f35fb749bde1
gmostafa1210 <gmostafa1210@gmail.com>
2026-09-22 15:38:17 +0600

    Remove period management and timetable features from the Madrashah Timetable module, including all related models, views, reports, and access rights. This cleanup eliminates unused code and resources, streamlining the module's functionality.

--- algorid_madrashah_base/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-#################################################################
-#                                                               #
-#       Developed by Algorid Limited                            #
-#       Copyright (C) 2026 - Today (https://www.algorid.com)    #
-#       Contact us: contact@algorid.com                         #
-#                                                               #
-#################################################################
-from . import models
